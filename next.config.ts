82c3bd15ef6b2e62ef9aa1349f0e640690e4410e
Kyno <40477759+ImKyno@users.noreply.github.com>
2026-03-15 11:05:52 -0300

    fix basePath for the new domain
    * fix(paths.ts): Removed the basePath
    
    *fix(next.config.ts): Removed the basePath

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
-const basePath = "/Heap-of-Foods-Recipe-Book";
+// const basePath = "/Heap-of-Foods-Recipe-Book";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
+  // basePath,
+  // assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+    NEXT_PUBLIC_BASE_PATH: "", //basePath,
   },
 };
 
